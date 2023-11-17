635e8dac171a4f214054f860d11ace46d8fb1cb4
TheSicilian12 <mguidera314@gmail.com>
2023-11-17 13:41:08 -0500

    more vocab

---
+++ src/backend/vocab.js
@@ -0,0 +1,12 @@
+export let vocab = {
+    lesson1: {
+        1: {
+            spanish: 'como',
+            english: 'as'
+        },
+        2: {
+            spanish: 'I',
+            english: 'I'
+        }
+    }
+}
