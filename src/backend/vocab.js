59d06772056b7888a767ea85d64de3a4be561807
TheSicilian12 <mguidera314@gmail.com>
2023-11-17 13:52:43 -0500

    setting up a basic vocab list

--- src/backend/vocab.js
+++ src/backend/vocab.js
@@ -1,12 +1,134 @@
 export let vocab = {
     lesson1: {
-        1: {
-            spanish: 'como',
-            english: 'as'
+        vocab: {
+            1: {
+                spanish: 'como',
+                english: 'as'
+            },
+            2: {
+                spanish: 'I',
+                english: 'I'
+            },
+            3: {
+                spanish: 'su',
+                english: 'his'
+            },
+            4: {
+                spanish: 'que',
+                english: 'that'
+            },
+            5: {
+                spanish: 'él',
+                english: 'he'
+            },
+            6: {
+                spanish: 'era',
+                english: 'was'
+            },
+            7: {
+                spanish: 'para',
+                english: 'for'
+            },
+            8: {
+                spanish: 'en',
+                english: 'on'
+            },
+            9: {
+                spanish: 'son',
+                english: 'are'
+            },
+            10: {
+                spanish: 'con',
+                english: 'with'
+            }
         },
-        2: {
-            spanish: 'I',
-            english: 'I'
+        bonus: {
+            1: {
+                spanish: 'ellos',
+                english: 'they'
+            },
+            2: {
+                spanish: 'ser',
+                english: 'be'
+            },
+            3: {
+                spanish: 'en',
+                english: 'at'
+            },
+            4: {
+                spanish: 'uno',
+                english: 'one'
+            },
+            5: {
+                spanish: 'tener',
+                english: 'have'
+            },
+        }
+    },
+    lesson2: {
+        vocab: {
+            1: {
+                spanish: 'ellos',
+                english: 'they'
+            },
+            2: {
+                spanish: 'ser',
+                english: 'be'
+            },
+            3: {
+                spanish: 'en',
+                english: 'at'
+            },
+            4: {
+                spanish: 'uno',
+                english: 'one'
+            },
+            5: {
+                spanish: 'tener',
+                english: 'have'
+            },
+            6: {
+                spanish: 'este',
+                english: 'this'
+            },
+            7: {
+                spanish: 'desde',
+                english: 'from'
+            },
+            8: {
+                spanish: 'por',
+                english: 'by'
+            },
+            9: {
+                spanish: 'caliente',
+                english: 'hot'
+            },
+            10: {
+                spanish: 'palabra',
+                english: 'word'
+            }
+        },
+        bonus: {
+            1: {
+                spanish: 'pero',
+                english: 'but'
+            },
+            2: {
+                spanish: 'qué',
+                english: 'what'
+            },
+            3: {
+                spanish: 'algunos',
+                english: 'some'
+            },
+            4: {
+                spanish: 'es',
+                english: 'is'
+            },
+            5: {
+                spanish: 'lo',
+                english: 'it'
+            },
         }
     }
 }
